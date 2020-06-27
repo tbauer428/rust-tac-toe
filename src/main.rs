95035cf86389b772806d6fbbb44a65210bff0bac
Thomas <tom.bauer95@gmail.com>
2020-06-26 22:22:13 -0500

    added 21 option in main menu

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,52 @@
+mod twentyone;
+mod game;
+
 extern crate rand;
 
 use crate::game::Game;
+use crate::twentyone::Twentyone;
+use std::io;
 
-mod game;
 
 fn main() {
-    println!("Welcome to Tic-Tac-Toe!");
 
-    let mut game = Game::new();
+    let mut player_input = String::new();
 
-    game.play_game();
+    println!("Would you like to play Tic-Tac-Toe? Or 21?:  (1/2)");
+
+    match io::stdin().read_line(&mut player_input) {
+        Ok(_) => {
+
+            let temp = player_input.to_lowercase();
+
+            if temp.trim() == "1" {
+
+                println!("Welcome to Tic-Tac-Toe!");
+
+                let mut game = Game::new();
+
+                game.play_game();
+
+            }else if temp.trim() == "2" {
+
+                println!("Pardon our dust!");
+
+                let mut twentyone = Twentyone::new();
+
+                twentyone.play_game()
+
+            }
+
+        }
+        Err(_) => (),
+    }
+
+
+
+
+
+
+
+
+
 }
---
+++ src/twentyone.rs
@@ -0,0 +1,35 @@
+
+
+enum Turn {
+    Player,
+    Bot,
+}
+
+
+pub struct Twentyone {
+    current_turn: Turn,
+}
+
+// Pick a number for the dealer, pick a number for the player
+// If the dealer has 21 and the player does not -> dealer win
+
+
+
+impl Twentyone {
+    pub fn new() -> Twentyone {
+        Twentyone {
+            current_turn: Turn::Player,
+        }
+    }
+
+    pub fn play_game() {
+
+    }
+
+
+
+
+
+}
+
+
